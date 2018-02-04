6a8564548ddb4c49cd11f05e53899217b71df9a4
Benjamin Gudehus <hastebrot@gmail.com>
2018-02-04 03:08:03 +0100

    (feature) Demo: Adapt some option monad/applicative examples.

--- src/main/kotlin/hello/world.kt
+++ src/main/kotlin/hello/world.kt
@@ -3,8 +3,15 @@ package hello
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.Some
+import arrow.core.applicative
+import arrow.core.ev
 import arrow.core.getOrElse
+import arrow.core.monad
 import arrow.data.Try
+import arrow.syntax.applicative.map
+import arrow.syntax.option.none
+import arrow.typeclasses.binding
+import java.util.UUID
 
 inline val <T> Some<T>.value get() = this.t
 
@@ -35,6 +42,33 @@ fun main(vararg args: String) {
 
     run {
         Try<Int> { error("failed") }.map { it + 1 }.println
-        Try { 5 }.map { it + 1 }.println
+        Try { 2 }.map { it + 1 }.println
+    }
+
+    // FP in Kotlin with Arrow Options
+    // https://www.youtube.com/watch?v=5SFTbphderE
+
+    run {
+        val maybeOne = Option(1)
+//        val maybeTwo = Option(2)
+        val maybeTwo = none<Int>()
+        val maybeThree = Option(3)
+
+        Option.monad().binding {
+            val one = maybeOne.bind()
+            val two = maybeTwo.bind()
+            val three = maybeThree.bind()
+            one + two + three
+        }.ev().println
+    }
+
+    run {
+        val maybeId = Option(UUID.randomUUID())
+        val maybeName = Option("foo bar")
+        val maybeAge = Option(123)
+
+        Option.applicative().map(maybeId, maybeName, maybeAge, { (id, name, age) ->
+            id to name to age
+        }).ev().println
     }
 }
