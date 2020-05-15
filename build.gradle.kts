8a1c142e6c20a39b8a6a86c3d2fc165fae322c7c
Benjamin Gudehus <hastebrot@gmail.com>
2020-05-15 19:42:32 +0200

    (chore) Gradle: Upgrade to Gradle 6.4 and Kotlin 1.3.72.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.dsl.Coroutines
 
 val gradleWrapperVersion: String by project
 val kotlinVersion: String by project
 val arrowVersion by extra { "0.7.2" }
 
 plugins {
-    val kotlinVersion = "1.2.50"
-    val dokkaVersion = "0.9.17"
+    val kotlinVersion = "1.3.72"
+    val dokkaVersion = "0.10.1"
 
     kotlin("jvm") version kotlinVersion
     id("org.jetbrains.dokka") version dokkaVersion
@@ -19,34 +18,29 @@ repositories {
 }
 
 dependencies {
-    compile(kotlin("stdlib", kotlinVersion))
-    compile(kotlin("stdlib-jdk7", kotlinVersion))
-    compile(kotlin("stdlib-jdk8", kotlinVersion))
-    compile(kotlin("reflect", kotlinVersion))
+    implementation(kotlin("stdlib", kotlinVersion))
+    implementation(kotlin("stdlib-jdk7", kotlinVersion))
+    implementation(kotlin("stdlib-jdk8", kotlinVersion))
+    implementation(kotlin("reflect", kotlinVersion))
 
-    compile("io.arrow-kt:arrow-core:$arrowVersion")
-    compile("io.arrow-kt:arrow-syntax:$arrowVersion")
-    compile("io.arrow-kt:arrow-typeclasses:$arrowVersion")
-    compile("io.arrow-kt:arrow-data:$arrowVersion")
-    compile("io.arrow-kt:arrow-instances-core:$arrowVersion")
-    compile("io.arrow-kt:arrow-instances-data:$arrowVersion")
-    compile("io.arrow-kt:arrow-effects:$arrowVersion")
-    compile("io.arrow-kt:arrow-optics:$arrowVersion")
-//    compile("io.arrow-kt:arrow-recursion:$arrowVersion")
+    implementation("io.arrow-kt:arrow-core:$arrowVersion")
+    implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
+    implementation("io.arrow-kt:arrow-typeclasses:$arrowVersion")
+    implementation("io.arrow-kt:arrow-data:$arrowVersion")
+    implementation("io.arrow-kt:arrow-instances-core:$arrowVersion")
+    implementation("io.arrow-kt:arrow-instances-data:$arrowVersion")
+    implementation("io.arrow-kt:arrow-effects:$arrowVersion")
+    implementation("io.arrow-kt:arrow-optics:$arrowVersion")
 }
 
 dependencies {
-    testCompile(kotlin("test", kotlinVersion))
-    testCompile(kotlin("test-junit5", kotlinVersion))
+    testImplementation(kotlin("test", kotlinVersion))
+    testImplementation(kotlin("test-junit5", kotlinVersion))
 }
 
-//java {
-//    sourceCompatibility = JavaVersion.VERSION_1_8
-//    targetCompatibility = JavaVersion.VERSION_1_8
-//}
-
-kotlin {
-    experimental.coroutines = Coroutines.ENABLE
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks {
@@ -60,11 +54,6 @@ tasks {
     }
 
     withType<DokkaTask> {
-        outputFormat = "javadoc"
-        outputDirectory = "$buildDir/javadoc"
-    }
-
-    "dokkaJavadoc"(DokkaTask::class) {
         outputFormat = "javadoc"
         outputDirectory = "$buildDir/javadoc"
     }
